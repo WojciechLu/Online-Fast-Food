d5a58bb59cc14d74aa32521e744426eb32a12ef5
Wojciech Łuszczak <wojtekluszczakpl@gmail.com>
2022-07-02 09:09:51 +0200

    Init account interfaces, facade and service files

---
+++ Online-Fast-Food/OFF.Infrastructure.EntityFramework/Services/AccountSrv.cs
@@ -0,0 +1,12 @@
+﻿using OFF.Domain.Interfaces.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OFF.Infrastructure.EntityFramework.Services;
+
+public class AccountSrv : IAccountSrv
+{
+}
